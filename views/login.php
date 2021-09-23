a07cd451bf299e7c8c112344368a42f510b7b6e6
unknown <omar_erg@hotmail.com>
2021-09-22 20:37:22 -0500

    Mi aplicación con acceso a base de datos

--- views/login.php
+++ views/login.php
@@ -1,10 +1,36 @@
-<?php?>
+<?php
+
+include('./model/db.php');
+session_start();
+
+if (isset($_POST['login'])) {
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $query = mysqli_query($mysqli, "SELECT * FROM users WHERE Usuario='$username'");
+
+  $result = $query->fetch_assoc();
+
+  if (!$result) {
+      echo '<p class="error">Datos Erroneos</p>';
+  } else {
+      if (password_verify($password, $result['Pass'])) {
+          $_SESSION['user'] = $result['Usuario'];
+          echo '<p class="success">Logueo Exitoso</p>';
+      } else {
+          echo '<p class="error">Contraseña incorrecta</p>';
+      }
+  }
+}
+
+?>
 
 <div class="container">
   <div class="flexbox">
     <div class="row">
       <div class="col s12 m6 l4 offset-l4 offset-m3">
-        <form action="" method="get">
+        <form action="" method="post">
           <div class="row card-panel z-depth-4">
             <div class="input-field col s12">
               <i class="material-icons prefix">account_circle</i>
@@ -12,7 +38,7 @@
               type="text"
               placeholder="Usuario"
               id="usuario"
-              name="usuario"
+              name="username"
               class="validate"
               required
               />
@@ -23,12 +49,12 @@
               type="password"
               placeholder="Contraseña"
               id="pswd"
-              name="pswd"
+              name="password"
               class="validate"
               required
               />
             </div>
-            <button class="btn blue right">
+            <button class="btn blue right" name="login" type="submit" value="login">
               <i class="material-icons left">login</i>
               Enviar
             </button>
--- views/register.php
+++ views/register.php
@@ -1,9 +1,38 @@
-<?php?>
+<?php
+
+include('./model/db.php');
+session_start();
+
+if (isset($_POST['register'])) {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+    $query = mysqli_query($mysqli,"SELECT * FROM users WHERE Usuario = '$username'");
+    if (mysqli_num_rows($query) > 0) {
+        echo '<p class="error">El Nombre de usuario ya esta registrado</p>';
+    }
+
+    if (mysqli_num_rows($query) == 0) {
+        $result = mysqli_query($mysqli, "INSERT INTO users(Usuario,Pass) VALUES ('$username','$password_hash')");
+
+        if ($result) {
+            echo '<p class="success">Registro Exitoso</p>';
+        } else {
+            echo '<p class="error">Algo salio mal :c</p>';
+        }
+    }
+
+    $mysqli -> close();
+}
+
+?>
 
 <div class="container">
   <div class="row">
     <div class="col s12 m6 l4 offset-l4 offset-m3">
-      <form action="" method="get">
+      <form action="" method="post">
         <div class="row card-panel z-depth-4">
           <div class="input-field col s12">
             <i class="material-icons prefix">account_circle</i>
@@ -11,7 +40,7 @@
             type="text"
             placeholder="Usuario"
             id="usuario"
-            name="usuario"
+            name="username"
             class="validate"
             required
             />
@@ -22,7 +51,7 @@
             type="password"
             placeholder="Contraseña"
             id="pswd"
-            name="pswd"
+            name="password"
             class="validate"
             required
             />
@@ -38,7 +67,7 @@
             required
             />
           </div>
-          <button class="btn blue right">
+          <button class="btn blue right" type="submit" name="register" value="register">
             <i class="material-icons left">login</i>
             Enviar
           </button>
